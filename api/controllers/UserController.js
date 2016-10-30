6315f93711da06c03fd643bc3644b746eeb9623c
Rodrigo <rsibanez89@gmail.com>
2016-10-30 14:06:40 +1100

    Update the UserController, now you are login after you submit the registration form.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,17 +7,23 @@ _.merge(exports, {
   // Extend with custom logic here by adding additional fields, methods, etc.
 
   create: function (req, res, next) {
-    console.log("UserController.create  was called");
+    console.log("UserController.create was called");
 
     // the register method is defined in sails-auth/services/protocols/local.js
     sails.services.passport.protocols.local.register(req.body, function (err, user) {
       if (err)
         return res.negotiate(err);
 
-      return res.view('homepage',
-              {
-                  'registerd': user.username
-              });
+      sails.models.passport.findOne(user.id).exec(function (err, passport){
+          if (err) {
+            console.log(err);
+          }
+
+          req.session.authenticated = true;
+          req.session.passport = passport;
+
+          return sails.controllers.homepage.index(req, res, next);
+        });
     });
 
   },
