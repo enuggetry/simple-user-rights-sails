c4fe4e555c78e62ee24ccf644ca53ba5d6ac34d0
Rodrigo <rsibanez89@gmail.com>
2016-10-31 03:37:53 +1100

    Modified the config/bootstrap.js to generate and admin user and moderator user with admin rights.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,6 +11,36 @@
 
 module.exports.bootstrap = function(cb) {
 
+  var adminUser = {
+    username: 'rsibanez89',
+    email: 'rsibanez89@gmail.com',
+    password: 'Rsibanez89',
+    rights: '0'
+  };
+
+  sails.services.passport.protocols.local.register(adminUser, function (err, user) {
+    if (err) {
+      console.log(err);
+    }
+    else
+      sails.log.info('user', user, 'admin created successfully');
+  });
+
+  var moderatorUser = {
+    username: 'moderatorUser',
+    email: 'moderator@user.com',
+    password: '1moderator+',
+    rights: '1'
+  };
+
+  sails.services.passport.protocols.local.register(moderatorUser, function (err, user) {
+    if (err) {
+      console.log(err);
+    }
+    else
+      sails.log.info('user', user, 'moderator created successfully');
+  });
+
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
